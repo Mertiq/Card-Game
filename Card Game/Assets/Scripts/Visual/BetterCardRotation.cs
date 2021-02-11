b26f3f1f9515f67fe0948341c546de2632ddc549
Mert <mertiq@icloud.com>
2021-02-11 12:21:53 +0300

    Creating Card Template

---
+++ Card Game/Assets/Scripts/Visual/BetterCardRotation.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class BetterCardRotation : MonoBehaviour {
+
+    
+    public RectTransform CardFront;
+    public RectTransform CardBack;
+    public Transform targetFacePoint;
+    public Collider col;
+
+    private bool showingBack = false;
+
+	
+	void Update () 
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(origin: Camera.main.transform.position, 
+                                  direction: (-Camera.main.transform.position + targetFacePoint.position).normalized, 
+            maxDistance: (-Camera.main.transform.position + targetFacePoint.position).magnitude) ;
+        bool passedThroughColliderOnCard = false;
+        foreach (RaycastHit h in hits)
+        {
+            if (h.collider == col)
+                passedThroughColliderOnCard = true;
+        }
+
+        if (passedThroughColliderOnCard!= showingBack)
+        {
+            showingBack = passedThroughColliderOnCard;
+            if (showingBack)
+            {
+                CardFront.gameObject.SetActive(false);
+                CardBack.gameObject.SetActive(true);
+            }
+            else
+            {
+                CardFront.gameObject.SetActive(true);
+                CardBack.gameObject.SetActive(false);
+            }
+
+        }
+
+	}
+}
---
+++ Card Game/Assets/Scripts/Visual/OneCardManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OneCardManager : MonoBehaviour {
+
+    public CardAsset cardAsset;
+    public OneCardManager PreviewManager;
+    [Header("Text Component References")]
+    public Text NameText;
+    public Text ManaCostText;
+    public Text DescriptionText;
+    public Text HealthText;
+    public Text AttackText;
+    [Header("Image References")]
+    public Image CardTopRibbonImage;
+    public Image CardLowRibbonImage;
+    public Image CardGraphicImage;
+    public Image CardBodyImage;
+    public Image CardFaceFrameImage;
+    public Image CardFaceGlowImage;
+    public Image CardBackGlowImage;
+
+    void Awake()
+    {
+        if (cardAsset != null)
+            ReadCardFromAsset();
+    }
+
+    private bool canBePlayedNow = false;
+    public bool CanBePlayedNow
+    {
+        get
+        {
+            return canBePlayedNow;
+        }
+
+        set
+        {
+            canBePlayedNow = value;
+
+            CardFaceGlowImage.enabled = value;
+        }
+    }
+
+    public void ReadCardFromAsset()
+    {
+        if (cardAsset.characterAsset != null)
+        {
+            CardBodyImage.color = cardAsset.characterAsset.ClassCardTint;
+            CardFaceFrameImage.color = cardAsset.characterAsset.ClassCardTint;
+            CardTopRibbonImage.color = cardAsset.characterAsset.ClassRibbonsTint;
+            CardLowRibbonImage.color = cardAsset.characterAsset.ClassRibbonsTint;
+        }
+        else
+        {
+            //CardBodyImage.color = GlobalSettings.Instance.CardBodyStandardColor;
+            CardFaceFrameImage.color = Color.white;
+            //CardTopRibbonImage.color = GlobalSettings.Instance.CardRibbonsStandardColor;
+            //CardLowRibbonImage.color = GlobalSettings.Instance.CardRibbonsStandardColor;
+        }
+
+        NameText.text = cardAsset.name;
+
+        ManaCostText.text = cardAsset.ManaCost.ToString();
+
+        DescriptionText.text = cardAsset.Description;
+
+        CardGraphicImage.sprite = cardAsset.CardImage;
+
+        if (cardAsset.MaxHealth != 0)
+        {
+            AttackText.text = cardAsset.Attack.ToString();
+            HealthText.text = cardAsset.MaxHealth.ToString();
+        }
+
+        if (PreviewManager != null)
+        {
+            PreviewManager.cardAsset = cardAsset;
+            PreviewManager.ReadCardFromAsset();
+        }
+    }
+}
